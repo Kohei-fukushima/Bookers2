475d68841b2f0c40527b58492ed03a281b14f88f
KOHEI FUKUSHIMA <pecpec1129@gmail.com>
2022-05-23 11:03:12 +0000

    [Finish]bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,16 @@
 class BooksController < ApplicationController
-
+  before_action :check_user, only: [:edit, :update]
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    @books = Book.all
+    @user = current_user
+      if @book.save
+       redirect_to book_path(@book), notice: 'You have created book successfully.'
+      else
+       render :index
+      end
+
   end
 
   def index
@@ -15,6 +21,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def edit
@@ -23,8 +30,11 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+      if @book.update(book_params)
+       redirect_to book_path(@book.id), notice: 'You have updated book successfully.'
+      else
+       render :edit
+      end
   end
 
   def destroy
@@ -37,6 +47,14 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title,:body)
+  end
+
+  def check_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
+
   end
 
 end
