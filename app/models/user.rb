a69ece671e69b6e3316eb9496a42cc529f0a51cb
KOHEI FUKUSHIMA <pecpec1129@gmail.com>
2022-06-10 07:37:56 +0000

    [Finish]rating

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,6 @@ class User < ApplicationRecord
    has_many :followings, through: :relationships, source: :follower
    has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id
    has_many :followers, through: :reverse_of_relationships, source: :following
-   has_many :reviews, dependent: :destroy
 
     validates :name, uniqueness: true, length: { minimum: 2, maximum: 20 }
     validates :introduction,length: { maximum: 50 }
